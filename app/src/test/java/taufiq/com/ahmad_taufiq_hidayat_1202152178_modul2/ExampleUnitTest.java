982bf0fae7108478e6aeb24151b9c28b61643ca3
Ahmad <ataufiq665@gmail.com>
2018-02-17 21:53:24 +0700

    first commit

---
+++ app/src/test/java/taufiq/com/ahmad_taufiq_hidayat_1202152178_modul2/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package taufiq.com.ahmad_taufiq_hidayat_1202152178_modul2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
