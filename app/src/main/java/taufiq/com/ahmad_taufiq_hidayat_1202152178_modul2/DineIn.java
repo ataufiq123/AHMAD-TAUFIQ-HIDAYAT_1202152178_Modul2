1ec90196c99a3b70cf5802d927a032c0c0d38d8f
Ahmad <ataufiq665@gmail.com>
2018-02-18 19:25:40 +0700

    ADd Function for Intent to Dine In Or Take Away

---
+++ app/src/main/java/taufiq/com/ahmad_taufiq_hidayat_1202152178_modul2/DineIn.java
@@ -0,0 +1,13 @@
+package taufiq.com.ahmad_taufiq_hidayat_1202152178_modul2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DineIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dine_in);
+    }
+}
--- app/src/main/java/taufiq/com/ahmad_taufiq_hidayat_1202152178_modul2/MainActivity.java
+++ app/src/main/java/taufiq/com/ahmad_taufiq_hidayat_1202152178_modul2/MainActivity.java
@@ -1,7 +1,11 @@
 package taufiq.com.ahmad_taufiq_hidayat_1202152178_modul2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnPesan(View view) {
+
+    }
+
+    public void jenisPesanan(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_dine_in:
+                if (checked){
+                    Toast.makeText(this,"Anda Memilih Dine In",Toast.LENGTH_LONG).show();
+                    Intent a = new Intent(MainActivity.this,DineIn.class);
+                    startActivity(a);
+                }
+                    break;
+            case R.id.radio_take_away:
+                if (checked){
+                    Toast.makeText(this,"Anda Memilih Take Away",Toast.LENGTH_LONG).show();
+                    Intent a = new Intent(MainActivity.this,TakeAway.class);
+                    startActivity(a);
+                }
+                    break;
+        }
     }
 }
